fe89cbef3bf545d896c43481e8cc0935470ae774
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2024-10-14 16:42:05 -0700

    Update bsmr-init.py

--- Question1/bsmr-init.py
+++ Question1/bsmr-init.py
@@ -4,8 +4,8 @@ import time
 def open_replica_terminals(nodes_num, base_port, max_faulty_nodes):
     for i in range(0, nodes_num):
         command = f"python3 SMR.py {nodes_num} {base_port} {i} {max_faulty_nodes}"
-        #subprocess.Popen(["gnome-terminal", "--", "bash", "-c", command])
-        subprocess.Popen(["osascript", "-e", f'tell application "Terminal" to do script "{command}"'])
+        subprocess.Popen(["gnome-terminal", "--", "bash", "-c", command])
+        #subprocess.Popen(["osascript", "-e", f'tell application "Terminal" to do script "{command}"'])
 
 max_faulty_nodes = 1
 nodes_num = 3 * max_faulty_nodes + 1
