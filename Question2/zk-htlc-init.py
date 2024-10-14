b9db64703d8ed9935b14251a69723b4363ef6566
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2024-10-13 22:29:28 -0700

    Update zk-htlc-init.py

--- Question2/zk-htlc-init.py
+++ Question2/zk-htlc-init.py
@@ -1,13 +1,12 @@
 import subprocess
 import time
 
-def open_replica_terminals(nodes_num, base_port, max_faulty_nodes):
+def open_replica_terminals(nodes_num, base_port):
     for i in range(1, nodes_num+1):
-        command = f"python3 ZK-HTLC.py {nodes_num} {base_port} {i} {max_faulty_nodes}"
+        command = f"python3 ZK-HTLC.py {nodes_num} {base_port} {i}"
         #subprocess.Popen(["gnome-terminal", "--", "bash", "-c", command])
         subprocess.Popen(["osascript", "-e", f'tell application "Terminal" to do script "{command}"'])
 
-max_faulty_nodes = 1
-nodes_num = 3 * max_faulty_nodes + 1
-base_port = 5040
-open_replica_terminals(nodes_num, base_port, max_faulty_nodes)
+nodes_num = 4
+base_port = 5050
+open_replica_terminals(nodes_num, base_port)
